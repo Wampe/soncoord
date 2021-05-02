581f2699d81388a7d65c82dc609679528eef49d7
Dennis Wambach <4699636+Wampe@users.noreply.github.com>
2021-05-02 21:18:58 +0200

    Initital Project commit

---
+++ Soncoord.Client.WPF/Bootstrapper.cs.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using Soncoord.Client.WPF.Modules.Shell;
+using System.Windows;
+
+namespace Soncoord.Client.WPF
+{
+    class Bootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ Soncoord.Client.WPF/Modules/Shell/Shell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Soncoord.Client.WPF.Modules.Shell
+{
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
