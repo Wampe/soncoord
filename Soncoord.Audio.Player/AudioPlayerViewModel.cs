4224c9c331f24f83db87fb28ff8d5ac1ea8747e5
Dennis Wambach <4699636+Wampe@users.noreply.github.com>
2021-05-04 00:50:50 +0200

    Audio Position TimeSpan

--- Soncoord.Audio.Player/AudioPlayerViewModel.cs
+++ Soncoord.Audio.Player/AudioPlayerViewModel.cs
@@ -4,6 +4,7 @@ using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Threading;
 
 namespace Soncoord.Audio.Player
 {
@@ -15,11 +16,18 @@ namespace Soncoord.Audio.Player
         private Mp3FileReader _clickReader;
         private Mp3FileReader _songReader;
 
+        private readonly DispatcherTimer _positionTimer;
+
         public AudioPlayerViewModel()
         {
+            _positionTimer = new DispatcherTimer(DispatcherPriority.ApplicationIdle)
+            {
+                Interval = TimeSpan.FromMilliseconds(50)
+            };
+            _positionTimer.Tick += PositionTimerTick;
+
             ModuleTitle = "Audio Player 0.1";
             Devices = new ObservableCollection<DirectSoundDeviceInfo>();
-
             StopCommand = new DelegateCommand(StopCommandExecute, CanStopCommandExecute)
                 .ObservesProperty(() => IsPlaying);
             PlayCommand = new DelegateCommand(PlayCommandExecute, CanPlayCommandExecute)
@@ -27,12 +35,20 @@ namespace Soncoord.Audio.Player
                 .ObservesProperty(() => SelectedClickDevice)
                 .ObservesProperty(() => SelectedSongDevice);
 
+            UpdateAudioPosition(new TimeSpan(0));
             LoadDevices();
         }
 
         public DelegateCommand PlayCommand { get; set; }
         public DelegateCommand StopCommand { get; set; }
         public ObservableCollection<DirectSoundDeviceInfo> Devices { get; set; }
+
+        private TimeSpan _audioPosition;
+        public TimeSpan AudioPosition
+        {
+            get => _audioPosition;
+            set => SetProperty(ref _audioPosition, value);
+        }
 
         private string _moduleTitle;
         public string ModuleTitle
@@ -71,9 +87,9 @@ namespace Soncoord.Audio.Player
 
         private void PlayCommandExecute()
         {
-            _clickReader = new Mp3FileReader(@"PathToClickFile");
-            _songReader = new Mp3FileReader(@"PathToSongFile");
-
+            _clickReader = new Mp3FileReader(@"");
+            _songReader = new Mp3FileReader(@"");
+            
             _outputClick = new DirectSoundOut(SelectedClickDevice.Guid);
             _outputClick.PlaybackStopped += OnPlaybackStopped;
             _outputClick.Init(new OffsetSampleProvider(_clickReader.ToSampleProvider())
@@ -87,13 +103,14 @@ namespace Soncoord.Audio.Player
             {
                 DelayBy = _clickReader.TotalTime - _songReader.TotalTime + TimeSpan.FromSeconds(1)
             });
-            
+
             _outputClick.Play();
             _outputSong.Play();
+            _positionTimer.Start();
 
             IsPlaying = true;
         }
-
+        
         private bool CanStopCommandExecute()
         {
             return IsPlaying;
@@ -103,6 +120,8 @@ namespace Soncoord.Audio.Player
         {
             _outputClick?.Stop();
             _outputSong?.Stop();
+
+            _positionTimer.Stop();
         }
 
         private void OnPlaybackStopped(object sender, StoppedEventArgs e)
@@ -122,6 +141,7 @@ namespace Soncoord.Audio.Player
             _songReader = null;
 
             IsPlaying = false;
+            UpdateAudioPosition(new TimeSpan(0));
         }
 
         private void LoadDevices()
@@ -130,6 +150,16 @@ namespace Soncoord.Audio.Player
             {
                 Devices.Add(device);
             }
+        }
+
+        private void PositionTimerTick(object sender, EventArgs e)
+        {
+            UpdateAudioPosition(_songReader.CurrentTime);
+        }
+
+        private void UpdateAudioPosition(TimeSpan value)
+        {
+            AudioPosition = value;
         }
     }
 }
